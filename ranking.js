b092dabcab4bd3afd97827460b8bd9e218680dc8
AlexWilsonNC <alexwilson0017@yahoo.com>
2022-01-10 09:43:20 -0500

    removed today

--- ranking.js
+++ ranking.js
@@ -175,6 +175,9 @@ sortName.addEventListener('click', () => {
   sortName.style.backgroundColor = 'rgb(151, 14, 14)';
   sortCsPoints.style.backgroundColor = 'rgb(206, 46, 46)';
   sortAttendance.style.backgroundColor = 'rgb(206, 46, 46)';
+  sortName.style.border = '2px solid rgba(0,0,0,0.5';
+  sortCsPoints.style.border = 'none';
+  sortAttendance.style.border = 'none';
 });
 function sort_array_by_name (array, sort, desc) {
   array.sort(function (a, b) {
@@ -195,6 +198,9 @@ sortAttendance.addEventListener('click', () => {
   sortName.style.backgroundColor = 'rgb(206, 46, 46)';
   sortCsPoints.style.backgroundColor = 'rgb(206, 46, 46)';
   sortAttendance.style.backgroundColor = 'rgb(151, 14, 14)';
+  sortAttendance.style.border = '2px solid rgba(0,0,0,0.5';
+  sortCsPoints.style.border = 'none';
+  sortName.style.border = 'none';
 });
 function sort_array_by (array, sort, desc) {
   array.sort(function (a, b) {
@@ -215,6 +221,9 @@ sortCsPoints.addEventListener('click', () => {
   sortName.style.backgroundColor = 'rgb(206, 46, 46)';
   sortCsPoints.style.backgroundColor = 'rgb(151, 14, 14)';
   sortAttendance.style.backgroundColor = 'rgb(206, 46, 46)';
+  sortCsPoints.style.border = '2px solid rgba(0,0,0,0.5';
+  sortName.style.border = 'none'; 
+  sortAttendance.style.border = 'none'; 
 });
 function sort_array_by_cs (array, sort, desc) {
   array.sort(function (a, b) {
